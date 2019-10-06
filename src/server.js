c9bd727456d9c123da243a361806740cb4ee30e8
pabloppizarro <pablo.pizarro@outlook.com>
2019-10-05 21:12:28 -0300

    fixed comments

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.disable('x-powered-by')
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
-app.use(morgan('dev')) // muestra los logs de las rutas
+app.use(morgan('dev')) // show http logs
 app.use('/api', router)
 
 const log = (req, res, next) => {
@@ -21,11 +21,7 @@ const log = (req, res, next) => {
 
 app.use(log)
 
-// router.get('/me', (req, res) => {
-//   res.send({ message: 'Hello course' })
-// })
-
-// forma simplificada
+// router way
 router
   .route('/me')
   .get((req, res) => {
@@ -38,7 +34,7 @@ router
   .get()
   .post()
 
-// CRUD sin usar router
+// CRUD without router
 app.post('/', (req, res) => {
   console.log(req.body)
   res.send({ message: 'ok', data: req.body })
