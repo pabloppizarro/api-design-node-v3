fe212b1d911246e6d51442e94e0e132cc20fe670
pabloppizarro <pablo.pizarro@outlook.com>
2019-10-05 21:05:33 -0300

    test basic express router

--- src/server.js
+++ src/server.js
@@ -4,12 +4,48 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(morgan('dev')) // muestra los logs de las rutas
+app.use('/api', router)
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('loggin middleware', req)
+  next()
+}
+
+app.use(log)
+
+// router.get('/me', (req, res) => {
+//   res.send({ message: 'Hello course' })
+// })
+
+// forma simplificada
+router
+  .route('/me')
+  .get((req, res) => {
+    res.send({ message: 'Hello course' })
+  })
+  .post()
+
+router
+  .route('/me/:id')
+  .get()
+  .post()
+
+// CRUD sin usar router
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok', data: req.body })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
